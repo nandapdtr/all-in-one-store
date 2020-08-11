5ce2473d2a87d77b7616ceeb8213a64b00db2109
x014378 <nanda.rabba@klm.com>
2020-08-11 23:35:40 +0200

    added node server to serve angular application

---
+++ server/index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+
+app.use(express.static(process.cwd() + '/dist/all-in-one-store'));
+
+// set the port
+
+app.set('port', 3000);
+
+//Configuring routes
+
+app.get('/', (req, res) => {
+    res.render('index.html');
+});
+
+// listen on port
+
+app.listen(app.get('port'), () => {
+    console.log("Node app is running at localhost:" + app.get('port'));
+});
